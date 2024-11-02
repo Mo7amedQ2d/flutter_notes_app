d16108d4c8e996f23ba3983cc926eb68a50f649e
Mohamed Qayed <145319237+Mo7amedQ2d@users.noreply.github.com>
2024-11-02 19:03:32 +0300

    take input and validate date

--- lib/view/widgets/w_custom_buttom.dart
+++ lib/view/widgets/w_custom_buttom.dart
@@ -1,20 +1,23 @@
-
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, this.onTap});
 
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-          color: Colors.blueGrey, borderRadius: BorderRadius.circular(8)),
-      child: const Center(
-        child: Text(
-          'Save',
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+            color: Colors.blueGrey, borderRadius: BorderRadius.circular(8)),
+        child: const Center(
+          child: Text(
+            'Save',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
