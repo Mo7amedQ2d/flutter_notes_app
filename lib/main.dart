a84dd79deddd0804f00bf15891edd8f013c30261
Mohamed Qayed <145319237+Mo7amedQ2d@users.noreply.github.com>
2024-11-05 09:11:45 +0300

    add provider to cubit, multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notes_app/constants.dart';
+import 'package:flutter_notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:flutter_notes_app/model/note_model.dart';
 import 'package:flutter_notes_app/view/notes_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -16,12 +18,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      home: const Scaffold(
-        body: NotesView(),
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => AddNoteCubit()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+          home: const Scaffold(
+            body: NotesView(),
+          ),
+        ));
   }
 }
