345160103be1e25d6047bd474a898f3354ef0d19
Mohamed Qayed <145319237+Mo7amedQ2d@users.noreply.github.com>
2024-11-09 10:55:30 +0300

    end color list view for add note

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,15 +1,16 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_notes_app/constants.dart';
 import 'package:flutter_notes_app/model/note_model.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:meta/meta.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color? color = const Color(0xFF2A3439);
   addNote(NoteModel note) async {
+    note.color = color!.value;
     emit(AddNoteLoading());
 
     try {
--- lib/view/widgets/w_color_list_view.dart
+++ lib/view/widgets/w_color_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
@@ -31,15 +34,13 @@ class ColorListView extends StatefulWidget {
 }
 
 class _ColorListViewState extends State<ColorListView> {
-  int currentIndex = 2;
+  int currentIndex = 3;
 
   List<Color> colors = [
-    const Color(0xFFFFB703),
     const Color(0xFF2F4F4F),
     const Color(0xFF2A3439),
     const Color(0xFF6082B6),
     const Color(0xFFfb8500),
-    const Color(0xFF8ecae6),
     const Color(0xFF219ebc),
   ];
   @override
@@ -55,6 +56,7 @@ class _ColorListViewState extends State<ColorListView> {
               child: GestureDetector(
                 onTap: () {
                   currentIndex = index;
+                  BlocProvider.of<AddNoteCubit>(context).color = colors[index];
                   setState(() {});
                 },
                 child: ColorItem(
